7c7f98226edabff088606475a5d20d28f4b4c3d5
anjarupnik <anjarupnik@hotmail.com>
2018-01-15 16:25:02 +0100

    added some basic styling

--- src/rsvps/RsvpItem.js
+++ src/rsvps/RsvpItem.js
@@ -10,9 +10,10 @@ class RsvpList  extends PureComponent {
       const { rsvp } = this.props
       return (
         <div className="rsvpItem">
-          <img src={ rsvp.member.photo } alt="member"/>
+          <img src={ rsvp.member.photo ? rsvp.member.photo : 'https://t4.ftcdn.net/jpg/01/17/95/91/240_F_117959178_mOp22kjhdhWdoSoePHPafN7GLUYyvFNY.jpg'} alt="member"/>
+          <h5>{ rsvp.member.member_name }</h5>
+          <br />
           <a href={rsvp.event.event_url} target="_blank">{ rsvp.event.event_name }</a>
-          { rsvp.member.member_name }
         </div>
       )
     }
